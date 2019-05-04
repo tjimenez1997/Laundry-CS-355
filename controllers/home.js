13f53cf6d439cb0b0d254b9243a8c509d9c36c9b
Thomas Jimenez <thomasjimenez1997@gmail.com>
2019-05-04 22:47:27 +0000

    Added 2 Models, Fixed Sign In/Logout Added Orders and Workers Table Fixed Login/Logout Allow logins for both tasker and customers Fixed tasker login

--- controllers/home.js
+++ controllers/home.js
@@ -16,44 +16,79 @@ router.get('/', (req, res) => {
 router.get('/signout', passport.authenticate('jwt', { session: false }),
     //Everything in this function only occurs if user token is valid
     function(req, res) {
-        res.clearCookie('authToken');
+        res.clearCookie('authToken'); //In the future use refresh token?
         res.redirect('/');
     }
 );
 
-//Homepage Sign In POST Route (Future todo: Change route after signin to proper page, As well as failure pages.)
+//Homepage Sign In POST Route (Future todo: Change route after signin to proper page, As well as failure pages. Also, allow login for both workers to occur) emailAddress/userPassword
 router.post('/', (req, res) => {
   let input = req.body;
+  let customerLogin = true;
 
-  models.Customer.findOne({
+  models.Workers.findOne({
     where:{
         email: input.emailAddress
     }
   })
-  .then((customer) => {
-    if(!customer){
-       return res.status(401).send({
-            message: 'Authentication failed. User not found.',
-          });
+  .then((workers) => {
+    if(workers){
+       customerLogin = false;
     }
-     customer.comparePassword(req.body.userPassword, (err, isMatch) => {
-          console.log("Password: "+req.body.userPassword + "Matched Password: "+isMatch);
-          let pk = 'MIIBOAIBAAJAVPfYwkIYRZ6CUtRQFefcPD9p9GXn8e/capeB6RWkZtE0HTJpAms/Fa6TC8sUIYfC+hlD5Le1ORiL9VPKtydk0wIDAQABAkBU8OB0cnapQmiuPSlCfOOiJxLZC/bv2gXTWVq5lLUhQuOIXEqeQcxcTdNru0ki9C/tRmhcnaT5hlbr/7WDM2EhAiEAl0u3dpB+Y+QK8jp7iGlFPYA2COV0v9n8twfdv2ufBEUCIQCPxTYqF7lkjkStrMH7ysak6uG5PhnckgB+WFvDuwgyNwIgFjONM3/WnC/tj0gXspfIClNTGpEZRcHmPLGRz7IqPoUCIDvIkgFm5BzAXCasE+4UIA4r7bkN7csemz3umBoICBx5AiBzB9RHgQCGx4C4S3eaGI6dmor/A1j/Q4fsHkPs3KSCHQ==';
-          if(isMatch && !err) {
-            var token = jwt.sign(JSON.parse(JSON.stringify({firstname:customer.firstname, lastname:customer.lastname, email:customer.email, phone:customer.phone, address: customer.address})), pk, {expiresIn: 86400 * 30});
-            
-            //Store jwt with client side cookie
-            //Redirect to debug for now (should be changed to first customer route later)
-            res.cookie('authToken',token);
-            res.redirect('/debug');
-          } else {
-            res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
-          }
-        })
-      })
-      .catch((error) => res.status(400).send(error));
-});
 
+    if(customerLogin) {
+          models.Customer.findOne({
+              where:{
+                  email: input.emailAddress
+              }
+          })
+          .then((customer) => {
+              if(!customer){
+                  return res.status(401).send({message: 'Authentication failed. User not found.'});
+              }
+              customer.comparePassword(req.body.userPassword, (err, isMatch) => {
+                  console.log("Password: "+req.body.userPassword + "Matched Password: "+isMatch);
+                  let pk = 'MIIBOAIBAAJAVPfYwkIYRZ6CUtRQFefcPD9p9GXn8e/capeB6RWkZtE0HTJpAms/Fa6TC8sUIYfC+hlD5Le1ORiL9VPKtydk0wIDAQABAkBU8OB0cnapQmiuPSlCfOOiJxLZC/bv2gXTWVq5lLUhQuOIXEqeQcxcTdNru0ki9C/tRmhcnaT5hlbr/7WDM2EhAiEAl0u3dpB+Y+QK8jp7iGlFPYA2COV0v9n8twfdv2ufBEUCIQCPxTYqF7lkjkStrMH7ysak6uG5PhnckgB+WFvDuwgyNwIgFjONM3/WnC/tj0gXspfIClNTGpEZRcHmPLGRz7IqPoUCIDvIkgFm5BzAXCasE+4UIA4r7bkN7csemz3umBoICBx5AiBzB9RHgQCGx4C4S3eaGI6dmor/A1j/Q4fsHkPs3KSCHQ==';
+                  if(isMatch && !err) {
+                        var token = jwt.sign(JSON.parse(JSON.stringify({firstname:customer.firstname, lastname:customer.lastname, email:customer.email, phone:customer.phone, address: customer.address})), pk, {expiresIn: 86400 * 30});
+                    
+                        //Store jwt with client side cookie
+                        //Redirect to debug for now (should be changed to first customer route later)
+                        res.cookie('authToken',token);
+                        res.redirect('/debug');
+                  } else {
+                    res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+                  }
+                })
+              }).catch((error) => res.status(400).send(error));
+    } else {
+           models.Workers.findOne({
+              where:{
+                  email: input.emailAddress
+              }
+          })
+          .then((workers) => {
+              if(!workers){
+                  return res.status(401).send({message: 'Authentication failed. User not found.'});
+              }
+              workers.comparePassword(req.body.userPassword, (err, isMatch) => {
+                  console.log("Password: "+req.body.userPassword + "Matched Password: "+isMatch);
+                  let pk = 'MIIBOAIBAAJAVPfYwkIYRZ6CUtRQFefcPD9p9GXn8e/capeB6RWkZtE0HTJpAms/Fa6TC8sUIYfC+hlD5Le1ORiL9VPKtydk0wIDAQABAkBU8OB0cnapQmiuPSlCfOOiJxLZC/bv2gXTWVq5lLUhQuOIXEqeQcxcTdNru0ki9C/tRmhcnaT5hlbr/7WDM2EhAiEAl0u3dpB+Y+QK8jp7iGlFPYA2COV0v9n8twfdv2ufBEUCIQCPxTYqF7lkjkStrMH7ysak6uG5PhnckgB+WFvDuwgyNwIgFjONM3/WnC/tj0gXspfIClNTGpEZRcHmPLGRz7IqPoUCIDvIkgFm5BzAXCasE+4UIA4r7bkN7csemz3umBoICBx5AiBzB9RHgQCGx4C4S3eaGI6dmor/A1j/Q4fsHkPs3KSCHQ==';
+                  if(isMatch && !err) {
+                        var token = jwt.sign(JSON.parse(JSON.stringify({firstname:workers.firstname, lastname:workers.lastname, email:workers.email, phone:workers.phone, address: workers.address})), pk, {expiresIn: 86400 * 30});
+                    
+                        //Store jwt with client side cookie
+                        //Redirect to debug for now (should be changed to first customer route later)
+                        res.cookie('authToken',token);
+                        res.redirect('/debug');
+                  } else {
+                    res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+                  }
+                })
+              }).catch((error) => res.status(400).send(error));
+     }
+   })
+});
 
 
 module.exports = router;
--- controllers/sign-up.js
+++ controllers/sign-up.js
@@ -10,11 +10,22 @@ router.get('/', (req, res) => {
 //Register Page POST Route
 router.post('/', (req, res) => {
   let input = req.body;
-  models.Customer.create({firstname: input.firstName, lastname: input.lastName, email: input.emailAddress, phone: input.phoneNumber, password: input.password, address: input.address}).then(function(){
-    res.json({
-    msg: "Successfully added account to database. (Todo: Figure out how front end will display success or fail message to user/Input validation/Proper password security practices)"
-  });
-  });
+  let taskerSignup = input.taskerFirstName;
+  if(taskerSignup){
+  		models.Workers.create({firstname: input.taskerFirstName, lastname: input.taskerLastName, email: input.taskerEmailAddress, phone: input.taskerPhoneNumber, password: input.taskerPassword, address: input.taskerAddress})
+  		.then(function(){
+    		res.json({ msg: "Successfully added worker account to database." });
+  		});
+  } else { 
+  		models.Customer.create({firstname: input.firstName, lastname: input.lastName, email: input.emailAddress, phone: input.phoneNumber, password: input.password, address: input.address})
+  		.then(function(){
+    		res.json({ msg: "Successfully added customer account to database." });
+  		});
+  }
+
+
+
+ 
 });
 
 

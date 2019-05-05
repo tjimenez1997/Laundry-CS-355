0ce54036c32d06e3f5dab2293e06e4c04bc59adb
Robert Gajda <49698965+rgajda100@users.noreply.github.com>
2019-05-05 14:37:23 -0400

    Update account.js

--- controllers/account.js
+++ controllers/account.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const models = require('../models');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+require('../config/passport.js')(passport); 
 
 const router = express.Router();
 
@@ -8,6 +12,14 @@ router.get('/', (req, res) => {
   res.render('account');
 });
 
+//Protected Sign Out Get Route (You can only access if you are signed in)
+router.get('/signout', passport.authenticate('jwt', { session: false }),
+    //Everything in this function only occurs if user token is valid
+    function(req, res) {
+        res.clearCookie('authToken'); //In the future use refresh token?
+        res.redirect('/');
+    }
+)
 
 
 module.exports = router;

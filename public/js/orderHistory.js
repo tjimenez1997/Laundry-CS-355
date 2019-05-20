b64f2fe1caccef6cf9d324d6b44e6fab7bda5096
Thomas Jimenez <thomasjimenez1997@gmail.com>
2019-05-20 16:41:51 +0000

    Added Simple Fetch for Order API Call on Order History Page Todo: Display via frontend

--- public/js/orderHistory.js
+++ public/js/orderHistory.js
@@ -1,1 +1,18 @@
-console.log(orders);
+fetch('/api/orders')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+     console.log('Fetch Error :-S', err);
+  });

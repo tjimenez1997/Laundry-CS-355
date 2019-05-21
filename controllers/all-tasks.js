038f9d41441d6bfabdcd9c40cc7e7d1fc088e1b3
Johnny Lao <jlao4394@gmail.com>
2019-05-21 18:38:56 -0400

    all tasks show all accepted jobs

--- controllers/all-tasks.js
+++ controllers/all-tasks.js
@@ -4,7 +4,34 @@ const models = require('../models');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('all-tasks');
+    models.Order.findAll({
+        where: {
+            WorkerEmail: req.user.email
+        },
+        include: [models.Customer]
+    }).then((orders) => {
+        const arr = [];
+
+        orders.forEach(function (o) {
+            var order = o.toJSON();
+            order.pickuptime = order.pickuptime.toLocaleString();
+
+            if (order.washdry) {
+                order.serviceText = 'Wash & Dry';
+            }
+
+            if (order.dryclean) {
+                if (order.serviceText) {
+                    order.serviceText += ', Dry Clean';
+                } else {
+                    order.serviceText = 'Dry Clean';
+                }
+            }
+            arr.push(order);
+        });
+
+        res.render('all-tasks', {orders: arr});
+    });
 });
   
   
